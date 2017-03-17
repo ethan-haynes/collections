e494dbbb76275ce44f9554109ea03bc5b05ec4d4
Ethan Haynes <ehaynes@g.harvard.edu>
2017-03-17 07:44:30 -0500

    added getters and function that returns polar radius

--- point/point.js
+++ point/point.js
@@ -1,5 +1,17 @@
 const Point = function(x_point=0, y_point=0) {
   const x = x_point
   const y = y_point
+
+  return {
+    get x {
+      return x
+    },
+    get y {
+      return y
+    },
+    get r {
+      return Math.sqrt(x*x + y*y)
+    }
+  }
 }
 module.exports = Point
