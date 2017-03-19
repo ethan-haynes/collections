5e98109e09f19346001e396fbc81606aca8c6fff
Ethan Haynes <ethanhaynes@Ethans-MacBook-Pro.local>
2017-03-19 18:06:12 -0500

    added theta and angleTo

--- point/point.js
+++ point/point.js
@@ -12,6 +12,16 @@ const Point = function(x_point=0, y_point=0) {
     /* get polar radius */
     get r() {
       return Math.sqrt(x*x + y*y)
+    },
+    /* returns the angle theta of an (x, y) point */
+    theta() {
+        return Math.atan2(y, x)
+    },
+    /* angle between two points */
+    angleTo(to) {
+        dx = to.x - x
+        dy = to.y - y
+        return Math.atan2(dy, dx)
     }
   }
 }
